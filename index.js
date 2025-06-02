b2d72d9f83d47de67d3ba0adce8326dce7d4ed1b
hussamshannan <67448518+hussamshannan@users.noreply.github.com>
2025-06-02 06:14:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 const { v4: uuidV4 } = require("uuid");
 
 app.set("view engine", "ejs");
@@ -16,15 +20,14 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-    socket.on("join-room", (roomId, userId) => {
-      socket.join(roomId);
-      socket.to(roomId).emit("user-connected", userId);
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
 
-      socket.on("disconnect", () => {
-        socket.to(roomId).emit("user-disconnected", userId);
-      });
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
     });
-      
+  });
 });
 
 server.listen(3000);
