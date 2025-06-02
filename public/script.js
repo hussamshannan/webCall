939fad89ea04c59a35934ca342b2f7f1aa8800c0
hussamshannan <67448518+hussamshannan@users.noreply.github.com>
2025-06-02 12:33:13 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,9 +2,6 @@ const socket = io("/");
 
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  host: "https://webcall-7f8y.onrender.com",
-  port: 443,
-  path: "/",
   secure: true,
 }); // uses default PeerJS server
 
