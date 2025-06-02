7b02f8a71d7315b1033bfd55fc106b964e68bff7
hussamshannan <67448518+hussamshannan@users.noreply.github.com>
2025-06-02 04:59:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const https = require("https");
+const server = https.createServer(options, app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*", // or limit to your domain
+    methods: ["GET", "POST"],
+  },
+});
+const socket = io("https://webcall-7f8y.onrender.com", {
+  transports: ["websocket"],
+});
+  
 const { v4: uuidV4 } = require("uuid");
 
 app.set("view engine", "ejs");
@@ -16,15 +26,15 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-    socket.on("join-room", (roomId, userId) => {
-      socket.join(roomId);
-      socket.to(roomId).emit("user-connected", userId);
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
 
-      socket.on("disconnect", () => {
-        socket.to(roomId).emit("user-disconnected", userId);
-      });
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
     });
-      
+  });
 });
 
-server.listen(3000);
+server.listen(443); // For HTTPS
+
